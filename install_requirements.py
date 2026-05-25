afe467142445ace1a34cb37bcdb4f0790b9e62d6
thom <pvthom@proton.me>
2026-05-26 00:02:12 +0200

    Update install_requirements.py

--- install_requirements.py
+++ install_requirements.py
@@ -1,13 +1,6 @@
 import subprocess
 import sys
-import os
-requirements = """
-opencv-python
-requests
-psutil
-"""
-with open("requirements.txt", "w") as file:
-    file.write(requirements.strip())
+
 subprocess.check_call([
     sys.executable,
     "-m",
